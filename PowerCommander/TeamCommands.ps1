683df1e5568902a1cddadaf307dd1fbfd736ebaf
Sam Petch <smpetch+github@gmail.com>
2021-04-13 17:31:07 +0100

    Added function for retreiving teams

---
+++ PowerCommander/TeamCommands.ps1
@@ -0,0 +1,49 @@
+using namespace KeeperSecurity.Sdk
+
+function Get-KeeperTeams {
+	<#
+		.Synopsis
+		Get Keeper Teams
+	
+		.Parameter Uid
+		Teams UID
+	
+		.Parameter Filter
+		Return matching Teams only
+	#>
+	[CmdletBinding()]
+	#[OutputType([Vault.SharedFolder[]])] 
+	Param (
+		[string] $Uid,
+		[string] $Filter
+	)
+	Begin {
+	}
+	
+	Process {
+		[Vault.VaultOnline]$Vault = $Script:Vault
+		if ($vault) {
+			[Vault.Team] $Teams = $null
+
+			if ($Uid) {
+				if ($Vault.TryGetTeam($uid, [ref]$Teams)) {
+					$Teams
+				}
+			} else {
+				foreach ($Team in $Vault.Teams) {
+					if ($Filter) {
+						$match = $($record.Uid, $Team.Name) | Select-String $Filter | Select-Object -First 1
+						if (-not $match) {
+							continue
+						}
+					}
+					$Team
+				}
+			}
+		} else {
+			Write-Error -Message "Not connected"
+		}
+	}
+	
+	End {}
+}
