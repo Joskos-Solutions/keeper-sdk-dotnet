fb3b9b23a4c9739854d55ec186376acec0ce566f
Sam Petch <smpetch+github@gmail.com>
2021-04-18 00:18:40 +0100

    Shared folder member permissions

--- PowerCommander/SharedFolderCommands.ps1
+++ PowerCommander/SharedFolderCommands.ps1
@@ -50,3 +50,96 @@ function Get-KeeperSharedFolders {
 	}
 }
 New-Alias -Name ksf -Value Get-KeeperSharedFolders
+
+function Add-KeeperFolderMember {
+	<#
+		.Synopsis
+		Adds member to Keeper Shared folder
+	
+		.Parameter SharedFolderID
+		ID for shared folder
+	
+		.Parameter TeamId
+		ID of team
+
+		.Parameter UserId
+		ID or email of user 
+	#>
+	[CmdletBinding()]
+	Param(
+		[Parameter()][string] $SharedFolderID,
+		[Parameter()][string] $TeamId,
+		[Parameter()][string] $UserId,
+		[Parameter()][switch] $ManageUsers,
+		[Parameter()][switch] $ManageRecords
+	)
+
+	if ($TeamId -and $UserId) {
+		Write-Error "TeamId and UserId params cannot be used together."
+		break
+	}
+
+	$options = $null
+	$options = New-Object KeeperSecurity.Vault.SharedFolderOptions
+	if ($ManageUsers) {
+		$options.ManageRecords = $true
+	}
+	if ($ManageRecords) {
+		$options.ManageUsers = $true
+	}
+
+	if ($TeamId) {
+		$IdType = 2
+	} else {
+		$IdType = 1
+	}
+
+	[Vault.VaultOnline]$Vault = $Script:Vault
+	if ($Vault) {
+		$task = $Vault.SyncDown()
+        $task.GetAwaiter().GetResult()  | Out-Null
+
+		$Task = $Vault.PutUserToSharedFolder($SharedFolderID,$UserID,$IdType,$options)
+		$task.GetAwaiter().GetResult() | Out-Null
+	}
+}
+
+function Remove-KeeperFolderMember {
+	<#
+		.Synopsis
+		Adds member to Keeper Shared folder
+	
+		.Parameter SharedFolderID
+		ID for shared folder
+	
+		.Parameter TeamId
+		ID of team
+
+		.Parameter UserId
+		ID or email of user 
+	#>
+	[CmdletBinding()]
+	Param(
+		[Parameter()][string] $SharedFolderID,
+		[Parameter()][string] $TeamId,
+		[Parameter()][string] $UserId
+	)
+
+	if ($TeamId -and $UserId) {
+		Write-Error "TeamId and UserId params cannot be used together."
+		break
+	}
+
+	if ($TeamId) {
+		$IdType = 2
+	} else {
+		$IdType = 1
+	}
+
+	[Vault.VaultOnline]$Vault = $Script:Vault
+	if ($Vault) {
+		$Vault | Get-Member
+		$Task = $Vault.RemoveUserFromSharedFolder($SharedFolderID,$UserID,$IdType)
+		$task.GetAwaiter().GetResult() | Out-Null
+	}
+}
